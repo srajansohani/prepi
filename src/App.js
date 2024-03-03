fd4c6e71819eacbba10fe00fd78274cd6567d147
srajansohani <srajansohani999@gmail.com>
2024-03-03 23:23:00 +0530

    init_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Session from '../src/routes/Session/index.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ return (
+    <>
+       <Routes>
+          <Route path="/session" element={<Session />} />
+       </Routes>
+    </>
+ );
+};
 
 export default App;
---
+++ src/routes/Session/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { WebcamCapture } from '../../components/WebcamCapture.js'
+
+function Session() {
+  return (
+    <div>
+     <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Session
